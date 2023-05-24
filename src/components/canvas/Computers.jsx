82856c92dc639ae2a1a8017384bdaa381b629369
Louie <sxl7953@rit.edu>
2023-05-24 00:19:35 -0400

    changed pics of gltf

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./planet2/untitled.glb");
   return (
     <mesh>
       <hemisphereLight intensity={1.5} groundColor="black" />
