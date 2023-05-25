4050269e43b87279b180d89e2c4c6420220d6f99
Louie <sxl7953@rit.edu>
2023-05-25 12:21:28 -0400

    update clickable link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,14 +5,18 @@ function Footer() {
     <footer
       className={`${styles.paddingX} w-full flex justify-center py-5 fixed bottom-0 z-20 bg-primary`}
     >
-      <a href="https://github.com/Iouie">
+      <a href="https://github.com/Iouie" target="_blank" rel="noreferrer">
         <img
           src={github}
           alt="github"
           className="object-contain h-10 w-10"
         ></img>
       </a>
-      <a href="https://www.linkedin.com/in/stanley-louie-523136127/">
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.linkedin.com/in/stanley-louie-523136127/"
+      >
         <img
           src={linkedin}
           alt="linkedin"
